3418c02b19ba0a445a906659401d295164c7d7c9
Chameleon2die4 <chameleon2die4@gmail.com>
2022-02-13 16:43:41 +0200

    Fix function

--- load.php
+++ load.php
@@ -3,16 +3,18 @@
 namespace WpRouter;
 
 //function load() {
-    if ( Utility::prerequisites_met(phpversion(), \get_bloginfo('version')) ) {
-        // we can continue. Load all supporting files and hook into wordpress
-        add_action('init', [ 'WpRouter/Utility', 'init' ], -100, 0);
-        add_action(Utility::INIT_HOOK, [ 'WpRouter/Page', 'init' ], 0, 0);
-        add_action(Utility::INIT_HOOK, [ 'WpRouter/WP_Router', 'init' ], 1, 0);
+//    if ( Utility::prerequisites_met(phpversion(), \get_bloginfo('version')) ) {
+if (function_exists('add_action')) {
+    // we can continue. Load all supporting files and hook into wordpress
+    add_action('init', [ 'WpRouter/Utility', 'init' ], -100, 0);
+    add_action(Utility::INIT_HOOK, [ 'WpRouter/Page', 'init' ], 0, 0);
+    add_action(Utility::INIT_HOOK, [ 'WpRouter/WP_Router', 'init' ], 1, 0);
 
-        // Sample page
-        add_action(Utility::INIT_HOOK, [ 'WpRouter/Sample', 'init' ], 1, 0);
-    } else {
-        // let the user know prerequisites weren't met
-        add_action('admin_head', [ 'WpRouter/Utility', 'failed_to_load_notices' ], 0, 0);
-    }
+    // Sample page
+    add_action(Utility::INIT_HOOK, [ 'WpRouter/Sample', 'init' ], 1, 0);
+}
+//    } else {
+//        // let the user know prerequisites weren't met
+//        add_action('admin_head', [ 'WpRouter/Utility', 'failed_to_load_notices' ], 0, 0);
+//    }
 //}
