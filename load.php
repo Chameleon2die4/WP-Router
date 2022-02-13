3978b5c43983fa4460cb7ca35a571cf1173d29f7
Chameleon2die4 <chameleon2die4@gmail.com>
2022-02-13 16:54:30 +0200

    Fix load

--- load.php
+++ load.php
@@ -2,19 +2,12 @@
 
 namespace WpRouter;
 
-//function load() {
-//    if ( Utility::prerequisites_met(phpversion(), \get_bloginfo('version')) ) {
 if (function_exists('add_action')) {
-    // we can continue. Load all supporting files and hook into wordpress
-    add_action('init', [ 'WpRouter/Utility', 'init' ], -100, 0);
-    add_action(Utility::INIT_HOOK, [ 'WpRouter/Page', 'init' ], 0, 0);
-    add_action(Utility::INIT_HOOK, [ 'WpRouter/WP_Router', 'init' ], 1, 0);
+    // Load all supporting files and hook into wordpress
+    add_action('init', [ 'WpRouter\Utility', 'init' ], -100, 0);
+    add_action(Utility::INIT_HOOK, [ 'WpRouter\Page', 'init' ], 0, 0);
+    add_action(Utility::INIT_HOOK, [ 'WpRouter\Router', 'init' ], 1, 0);
 
     // Sample page
-    add_action(Utility::INIT_HOOK, [ 'WpRouter/Sample', 'init' ], 1, 0);
+    add_action(Utility::INIT_HOOK, [ 'WpRouter\Sample', 'init' ], 1, 0);
 }
-//    } else {
-//        // let the user know prerequisites weren't met
-//        add_action('admin_head', [ 'WpRouter/Utility', 'failed_to_load_notices' ], 0, 0);
-//    }
-//}
